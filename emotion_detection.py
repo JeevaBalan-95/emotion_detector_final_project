7e41534dc60315b121d2689991e21c778d3b6566
Jeevabalan <mjeeva000@gmail.com>
2025-08-22 11:39:25 -0400

    Added formatted output

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,9 +7,24 @@ def emotion_detector(text_to_analyze):
     input_json =  {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(url, json=input_json, headers=headers)
-
     formatted_response = json.loads(response.text)
-    return formatted_response
+    
+    identified_emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    # print(f"Identified emotions are {identified_emotions}")
+    
+    dominant_emotion = None
+    dominant_emotion_score = -1
+    for an_emotion in identified_emotions.keys():
+        if dominant_emotion_score < identified_emotions[an_emotion]:
+            dominant_emotion_score = identified_emotions[an_emotion]
+            dominant_emotion = an_emotion
+
+    # print(dominant_emotion)
+    # print(dominant_emotion_score)
+    identified_emotions["dominant_emotion"] = dominant_emotion
+
+    return identified_emotions
+
 
 # if __name__ == "__main__":
-#     print(emotion_detector("I love this new technology."))
+#     emotion_detector("I love this new technology.")
